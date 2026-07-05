0826c33992573b3c4414511d7ffc100349ebf6d1
Shivam Vishwakarma <recxcoder@gmail.com>
2026-07-05 20:16:21 +0530

    feat: resolve bug

--- src/utils/ai.py
+++ src/utils/ai.py
@@ -6,14 +6,14 @@ load_dotenv()
 client = Groq()
 MODEL="llama-3.3-70b-versatile"
 
-def llm(messages: list, json_object: bool = False):
+def llm(messages: list, json_mode: bool = False) -> str:
     kwargs = {
         "model": MODEL,
         "messages": messages
     }
 
-    if json_object:
+    if json_mode:
         kwargs["response_format"] = {"type": "json_object"}
 
-    resonse = client.chat.completions.createe(**kwargs)
-    return response.choiced[0].message.content.strip()
+    response = client.chat.completions.create(**kwargs)
+    return response.choices[0].message.content.strip()
