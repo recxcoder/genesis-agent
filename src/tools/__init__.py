3b8c3822299a159e36a399d9edf7ce943dc87d02
Shivam Vishwakarma <recxcoder@gmail.com>
2026-07-10 18:55:33 +0530

    feat: remove todo tool

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,4 +1,3 @@
 from src.tools.search import search_web
 from src.tools.browse import browse_web
 from src.tools.judge import llm_judge
-from src.tools.todo import add_todos, mark_todo_done, check_todos, check_done_todos, print_status
